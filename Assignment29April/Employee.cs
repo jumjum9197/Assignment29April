9729652e094335fa0839fbb182ef97fa7d6b9e74
Olajumoke Sekoni <sekoniolajumoke@gmail.com>
2022-05-04 10:10:33 +0100

    Add project files.

---
+++ Assignment29April/Employee.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment29April
+{
+    public  class Employee
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public int Age { get; set; }
+
+        public Gender Gender;
+        public int ID { get; set; }
+
+        public static List<Employee> Main1()
+        {
+            return new List<Employee>
+            {
+                new Employee{ FirstName = "Jude", LastName ="Smith", Age = 41, Gender = Gender.Male , ID =001},
+                new Employee{ FirstName = "Sarah", LastName ="Jones", Age = 22, Gender = Gender.Female , ID =002},
+                new Employee{ FirstName = "Stacy", LastName ="Baker", Age = 21, Gender = Gender.Female , ID =003},
+                new Employee{ FirstName = "Samuel", LastName ="Dexter", Age = 17, Gender = Gender.Male , ID =004},
+                new Employee{ FirstName = "Richy", LastName ="Smith", Age = 49, Gender = Gender.Male , ID =005},
+                new Employee{ FirstName = "Brett", LastName ="joy", Age = 51, Gender = Gender.Female , ID =006},
+                new Employee{ FirstName = "Mark", LastName ="Parker", Age = 116, Gender = Gender.Male , ID =007},
+                new Employee{ FirstName = "Alice", LastName ="Thompson", Age = 18, Gender = Gender.Female , ID =008},
+                new Employee{ FirstName = "Evelyn", LastName ="Winson", Age = 58, Gender = Gender.Male , ID =009},
+                new Employee{ FirstName = "Mort", LastName ="Martin", Age = 58, Gender = Gender.Male , ID =010},
+                new Employee{ FirstName = "Eugene", LastName ="deLauter", Age = 84, Gender = Gender.Male , ID =011},
+                new Employee{ FirstName = "Gail", LastName ="Dude", Age = 34, Gender = Gender.Male , ID =012}
+            };
+           
+
+        }
+    }
+    public enum Gender
+    {
+        Male,
+        Female
+
+    }
+}
---
+++ Assignment29April/Program.cs
@@ -0,0 +1,118 @@
+﻿
+using System;
+using System.Collections;
+using System.Linq;
+namespace Assignment29April
+{
+    public class Jumoke
+    {
+        
+        static void Main()
+        {
+            var emp = Employee.Main1();
+            var lastName = emp.Where(x => x.LastName.StartsWith("D"));
+            foreach (var item in lastName)
+            {
+                Console.WriteLine($"lastname:{item.LastName}");
+            }
+
+            Console.WriteLine(      "/n ++++++++++++/n");
+            var older = emp.Where(o => o.Age > 40).OrderByDescending(o => o.FirstName);
+            var lastPerson = older.First();
+            Console.WriteLine($"age: {lastPerson.Age} name :{lastPerson.FirstName}+{lastPerson.LastName}");
+
+
+            Console.WriteLine("OOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOoo");
+
+            string[] words =
+            {
+
+            "abstract", "add*", "alias*", "as", "ascending*",
+                 "async*", "await*", "base","bool", "break",
+             "by*", "byte", "case", "catch", "char", "checked",
+             "class", "const", "continue", "decimal", "default",
+             "delegate", "descending*", "do", "double",
+             "dynamic*", "else", "enum", "event", "equals*",
+             "explicit", "extern", "false", "finally", "fixed",
+             "from*", "float", "for", "foreach", "get*", "global*",
+             "group*", "goto", "if", "implicit", "in", "int",
+             "into*", "interface", "internal", "is", "lock", "long",
+             "join*", "let*", "nameof*", "namespace", "new", "null",
+             "object", "on*", "operator", "orderby*", "out",
+             "override", "params", "partial*", "private", "protected",
+             "public", "readonly", "ref", "remove*", "return", "sbyte",
+             "sealed", "select*", "set*", "short", "sizeof",
+             "stackalloc", "static", "string", "struct", "switch",
+             "this", "throw", "true", "try", "typeof", "uint", "ulong",
+             "unsafe", "ushort", "using", "value*", "var*", "virtual",
+             "unchecked", "void", "volatile", "where*", "while", "yield*"
+
+
+
+
+            };
+            Console.WriteLine("888888888888888888888888");
+            var containE = words.Where(b => b.Contains("e")).OrderBy(b => b);
+
+            foreach (var word in containE)
+            {
+                Console.WriteLine(word);
+
+            }
+            Console.WriteLine("#############################33");
+
+
+            var lastword = containE.Last();
+
+
+            Console.WriteLine(lastword);
+
+            Console.WriteLine("))))))))))))))))))))))))))))))0000");
+
+
+            var select = words.Where(b => b.Contains("*")).OrderBy(b => b);
+            foreach (var word in select)
+            {
+
+                Console.WriteLine(word);
+            }
+            Console.WriteLine("Ppppppppppppp\n");
+
+
+
+            string[] nonEmpty = { "plane", "ferry", "car", "bike" };
+            var contain = nonEmpty.OrderBy(b => b)
+                .Where(b => b.Contains("e"));
+
+            //foreach (var a in contain)
+            //{
+            //    Console.WriteLine(a);
+
+            //}
+            var b = contain.First();
+
+            Console.WriteLine(b);
+
+
+
+
+
+
+        }
+
+
+
+    }  
+
+
+
+    
+
+}   
+      
+
+
+  
+
+
+
